0fda810eabaee7bf307ce24e8bfd0579a3f0e410
willmhough <whough20@nscds.org>
2020-01-21 20:59:40 +0000

    class

---
+++ Coding Challenge 1-21-20/function.js
@@ -0,0 +1,20 @@
+//Challenge 1
+
+function centuryFromYear(year) {
+    return Math.ceil(year/100);
+}
+
+console.log(centuryFromYear(1905));
+console.log(centuryFromYear(1700));
+
+//Challenge 2
+
+function reverse(message) {
+    var reverseString = "";
+    for(var i=message.length-1; i>=0; i--){
+        reverseString += message.charAt(i);
+    }
+    return reverseString;
+}
+console.log(reverse("hello"));
+console.log(reverse("good"));
