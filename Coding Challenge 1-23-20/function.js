dd1d686b5b804734321490986fe5f04160455a73
willmhough <whough20@nscds.org>
2020-01-23 19:14:12 +0000

    class

---
+++ Coding Challenge 1-23-20/function.js
@@ -0,0 +1,45 @@
+//Challenge 1
+
+function hasVowels(string){
+let word = string.toLowerCase()
+if(/[aeiouy]/.test(word)){
+     return true
+  } else {
+     return false
+  }
+}
+
+function noVowels() {
+    var vowels="aeiou";
+    var string="";
+    for(var i=message.length-1; i>=0; i--){
+    }
+}
+
+var message="Hello how are you? Nevermind I don't care.";
+vowels.indexOf('')
+message1.indexOf('')
+function removeVowels();{
+
+}
+
+function noVowels(message);
+
+function hasVowels(string){
+  let word = string.toLowerCase()
+  if(/[aeiou]/.test(word)){
+    return true
+  } else if (word.includes("a") && AIsAVowel(word)){
+    return true
+  }else if (word.includes("e") && EIsAVowel(word)){
+    return true
+  }else if (word.includes("i") && EIsAVowel(word)){
+    return true
+  }else if (word.includes("o") && IIsAVowel(word)){
+    return true
+  }else if (word.includes("u") && UIsAVowel(word)){
+    return true
+  } else {
+    return false
+  }
+}
