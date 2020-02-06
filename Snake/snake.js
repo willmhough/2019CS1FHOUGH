e2145ec17af7a6dd26760d5eea22be27244ab733
willmhough <whough20@nscds.org>
2020-02-06 16:31:05 +0000

    class

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,10 +1,36 @@
 const cvs = document.getElementById("snake");
 const ctx = cvs.getContext("2d");
 
-var canvas=document.getElementById("snake");
+function resizeGame() {
+    var gameArea = document.getElementById('gameArea');
+    var widthToHeight = 4 / 3;
+    var newWidth = window.innerWidth;
+    var newHeight = window.innerHeight;
+    var newWidthToHeight = newWidth / newHeight;
+    
+    if (newWidthToHeight > widthToHeight) {
+        newWidth = newHeight * widthToHeight;
+        gameArea.style.height = newHeight + 'px';
+        gameArea.style.width = newWidth + 'px';
+    } else {
+        newHeight = newWidth / widthToHeight;
+        gameArea.style.width = newWidth + 'px';
+        gameArea.style.height = newHeight + 'px';
+    }
+    
+    gameArea.style.marginTop = (-newHeight / 2) + 'px';
+    gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+    
+    var gameCanvas = document.getElementById('gameCanvas');
+    gameCanvas.width = newWidth;
+    gameCanvas.height = newHeight;
+}
+
+window.addEventListener('resize', resizeGame, false);
 
 // create the unit
 const box = 32;
+console.log("box");
 
 const foodImg = document.getElementById("snake");
 
@@ -62,16 +88,6 @@ function direction(event){
     }
 }
 
-function resizeGame() {
-    var height = $(window).height();
-    var width = $(window).width();
-    game.width = width;game.height = height;
-    game.stage.bounds.width = width;
-    game.stage.bounds.height = height;
-    if (game.renderType === Phaser.WEBGL){	game.renderer.resize(width, height);}
-}
-
-$(window).resize(function() { window.resizeGame(); } );
 
 // check collision function
 function collision(head,array){
